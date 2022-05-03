a133519f111f987005ae5aaa78429c0889a4c87e
Ivan Silva <ivankuartz@hotmail.com>
2022-05-03 12:20:50 -0300

    Uncommented INDEX solution

--- schema.sql
+++ schema.sql
@@ -63,4 +63,4 @@ ALTER TABLE visits ADD COLUMN date_of_visit DATE;
 
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
--- CREATE INDEX animals_id_asc ON visits(animals_id ASC); 
+CREATE INDEX animals_id_asc ON visits(animals_id ASC); 
